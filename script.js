0148df2d5b73cbefd02e63a9e7d65f224c2e6a21
amikeubakichunoi-cpu <amikeubakichunoi@gmail.com>
2026-04-03 22:46:38 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,57 @@
+let homeScoreEl = document.getElementById("score-home");
+let guestScoreEl = document.getElementById("score-guest");
+let homeScoreBtnOne = document.getElementById("home-score-btn-1");
+let homeScoreBtnTwo = document.getElementById("home-score-btn-2");
+let homeScoreBtnThree = document.getElementById("home-score-btn-3");
+let resetHomeScore = document.getElementById("reset-home");
+let resetGuestScore = document.getElementById("reset-guest");
+let guestScoreBtnOne = document.getElementById("guest-score-btn-1");
+let guestScoreBtnTwo = document.getElementById("guest-score-btn-2");
+let guestScoreBtnThree = document.getElementById("guest-score-btn-3");
+homeScoreBtnOne.addEventListener("click", function () {
+  homeScoreEl.textContent++;
+});
+
+// Button - 2 - Home
+
+homeScoreBtnTwo.addEventListener("click", function () {
+  homeScoreEl.textContent = Number(homeScoreEl.textContent) + 2;
+});
+
+// Button - 3 - Home
+
+homeScoreBtnThree.addEventListener("click", function () {
+  homeScoreEl.textContent = Number(homeScoreEl.textContent) + 3;
+});
+
+// Reset - Home
+
+resetHomeScore.addEventListener("click", function () {
+  homeScoreEl.textContent = 0;
+});
+
+// === Button Function - Guest === \\
+
+// Button - 1 - Guest
+
+guestScoreBtnOne.addEventListener("click", function () {
+  guestScoreEl.textContent++;
+});
+
+// Button - 2 - Guest
+
+guestScoreBtnTwo.addEventListener("click", function () {
+  guestScoreEl.textContent = Number(guestScoreEl.textContent) + 2;
+});
+
+// Button - 3 - Guest
+
+guestScoreBtnThree.addEventListener("click", function () {
+  guestScoreEl.textContent = Number(guestScoreEl.textContent) + 3;
+});
+
+// Reset - Guest
+
+resetGuestScore.addEventListener("click", function () {
+  guestScoreEl.textContent = 0;
+});
